728e54e1639c11277bb5d0efd3a79d8e8fe27d0f
Victor Lunes <victorllues@gmail.com>
2023-08-09 13:29:43 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application } from 'express';
+import { connect } from './infra/database';
+
+class App {
+  public app: Application;
+  constructor() {
+    this.app = express();
+    this.middlewaresInitialize();
+    this.initializeRoutes();
+    this.interceptionError();
+    connect();
+  }
+
+  initializeRoutes() {
+    //this.app.use('/', )
+  }
+
+  interceptionError(){
+    // this.app.use()
+  }
+
+  middlewaresInitialize() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  listen() {
+    this.app.listen(3333, () => console.log('server is running'));
+  }
+}
+export { App };
---
+++ src/infra/database.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    await mongoose.connect('mongodb+srv://victorllunes:O4dqr1btQgScY0wU@cluster0.kqzahcw.mongodb.net/hero-ticket')
+    console.log("Connect database success")
+  } catch (error) {
+    console.log("Error connecting to the database")
+  }
+}
